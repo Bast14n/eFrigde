aecbbfa5b3804129dbc7b1264afa632026eabc3f
Krzysztof Pieprzyca <pieprzycakrzysztof@gmail.com>
2019-05-27 20:43:37 +0200

    add navibar

---
+++ e-fridge/src/NaviBar/NaviBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NaviBar.css';
+import Button from "reactstrap/es/Button";
+
+export const NaviBar = () => (
+    <div className="App-navibar">
+        <Button className="App-button">Button 1</Button>
+        <Button className="App-button">Button 2</Button>
+        <Button className="App-button">Button 3</Button>
+        <Button className="App-button">Button 4</Button>
+        <Button className="App-button">Button 5</Button>
+    </div>
+);
+
--- e-fridge/src/NaviBar/NaviBar.test.js
+++ e-fridge/src/NaviBar/NaviBar.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import MainUser from './NaviBar';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<MainUser />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
