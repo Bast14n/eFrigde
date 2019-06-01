ac443f886623e402fae0c854a4849c105c7e9c56
Bast14n <sebastian.surowka@gmail.com>
2019-06-01 16:59:18 +0200

    deploy

---
+++ docs/precache-manifest.8b2e4d19b258df6220a42d60fcba7b77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c3482215dc74142aaf054ca1cb8b69",
+    "url": "/eFrigde/index.html"
+  },
+  {
+    "revision": "86ab1c7d78e5711126e8",
+    "url": "/eFrigde/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "cb659872f8f5c47fb2df",
+    "url": "/eFrigde/static/css/main.a9e8210b.chunk.css"
+  },
+  {
+    "revision": "86ab1c7d78e5711126e8",
+    "url": "/eFrigde/static/js/2.f85c4ce1.chunk.js"
+  },
+  {
+    "revision": "cb659872f8f5c47fb2df",
+    "url": "/eFrigde/static/js/main.00bc68ab.chunk.js"
+  },
+  {
+    "revision": "0d36ddc317e0021c6027",
+    "url": "/eFrigde/static/js/runtime~main.65e9ec89.js"
+  }
+]);
