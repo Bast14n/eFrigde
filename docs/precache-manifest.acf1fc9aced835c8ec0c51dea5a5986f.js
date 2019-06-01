145cd33fd24dfd1c8ca9fefa7a66e2095fdc7bcf
Bast14n <sebastian.surowka@gmail.com>
2019-06-01 16:45:49 +0200

    build

---
+++ docs/precache-manifest.acf1fc9aced835c8ec0c51dea5a5986f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996f757da1602ccb517898184bf065e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9165fae4532179348c56",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b6a51e4e3cba872a368d",
+    "url": "/static/css/main.a9e8210b.chunk.css"
+  },
+  {
+    "revision": "9165fae4532179348c56",
+    "url": "/static/js/2.d9200700.chunk.js"
+  },
+  {
+    "revision": "b6a51e4e3cba872a368d",
+    "url": "/static/js/main.93c285cd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
